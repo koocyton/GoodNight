8dba844368206b5d89ff9ce0a4b9737ebe4b4667
liuyi <liuyi@ushow.media>
2017-01-17 10:10:32 +0800

    增加 icon ，修改 ViewController

---
+++ GoodNight/RecordViewController.swift
@@ -0,0 +1,24 @@
+//
+//  FirstViewController.swift
+//  GoodNight
+//
+//  Created by 刘毅 on 2016/12/30.
+//  Copyright © 2016年 刘毅. All rights reserved.
+//
+
+import UIKit
+
+class RecordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+

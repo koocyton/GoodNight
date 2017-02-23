94bbaacab533083add448204004e977b0382adfd
liuyi <liuyi@ushow.media>
2017-02-24 01:53:39 +0800

    commit some file

--- GoodNight/ShoppingViewController.swift
+++ GoodNight/ShoppingViewController.swift
@@ -17,9 +17,14 @@ class ShoppingViewController: UIViewController, UIWebViewDelegate {
         // Do any additional setup after loading the view, typically from a nib.
 
         // request url
-        let url = URL(string: "https://github.com/");
-        let request = URLRequest(url: url!);
-        webView.loadRequest(request)
+        //let url = URL(string: "https://github.com/");
+        //let request = URLRequest(url: url!);
+        //webView.loadRequest(request)
+        
+        // load local html
+        let path = Bundle.main.path(forResource: "ShoppingView", ofType: "html")
+        let url = NSURL.fileURL(withPath: path!)
+        webView.loadRequest(URLRequest(url: url))
     }
 
     override func didReceiveMemoryWarning() {

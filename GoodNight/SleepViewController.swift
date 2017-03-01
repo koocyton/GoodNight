9d7988796c86e6bd52ef443c25b05c4a6028bbb9
liuyi <liuyi@ushow.media>
2017-03-01 22:20:32 +0800

    sleep 页面完成

--- GoodNight/SleepViewController.swift
+++ GoodNight/SleepViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class SleepViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
 
+    @IBOutlet weak var catalogCollectionView: UICollectionView!
+    
     @IBOutlet weak var coverCollectionView: UICollectionView!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        catalogCollectionView.dataSource = self
+        catalogCollectionView.delegate = self
 
         coverCollectionView.dataSource = self
         coverCollectionView.delegate = self
@@ -25,25 +30,44 @@ class SleepViewController: UIViewController, UICollectionViewDataSource, UIColle
         // Dispose of any resources that can be recreated.
     }
     
+    // 有多少个 cell
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        // catalog
+        if (collectionView == catalogCollectionView) {
+            return 18;
+        }
+
+        // default cover
         return 18;
     }
     
+    // 每个 cell 的处理
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
 
-        let coverCell = collectionView.dequeueReusableCell(withReuseIdentifier: "coverCell", for: indexPath) as! CoverCellContent
-        
-        coverCell.coverImage.image = UIImage(named:"normal")
-        coverCell.coverLabel.text = "\(indexPath.section):\(indexPath.row)"
+        // catalog
+        if (collectionView == catalogCollectionView) {
 
+            let catalogCell = collectionView.dequeueReusableCell(withReuseIdentifier: "catalogCell", for: indexPath) as! CatalogCell
+            catalogCell.catalogLabel.text = "\(indexPath.section):\(indexPath.row)"
+            return catalogCell
+        }
+
+        // default cover
+        let coverCell = collectionView.dequeueReusableCell(withReuseIdentifier: "coverCell", for: indexPath) as! CoverCell
+        coverCell.coverImage.image = UIImage(named:"normal")
         return coverCell
     }
 }
 
-class CoverCellContent: UICollectionViewCell {
+class CatalogCell: UICollectionViewCell {
+
+    @IBOutlet var catalogLabel: UILabel!
+    
+}
+
+class CoverCell: UICollectionViewCell {
 
     @IBOutlet var coverImage: UIImageView!
-
-    @IBOutlet var coverLabel: UILabel!
     
 }

ce9030acc6aa4fd0904db1b3591e51952ce1768f
liuyi <liuyi@ushow.media>
2017-03-02 18:43:32 +0800

    good job

--- GoodNight/SleepViewController.swift
+++ GoodNight/SleepViewController.swift
@@ -8,66 +8,101 @@
 
 import UIKit
 
-class SleepViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+class SleepViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UIScrollViewDelegate {
 
-    @IBOutlet weak var catalogCollectionView: UICollectionView!
-    
-    @IBOutlet weak var coverCollectionView: UICollectionView!
-    
+    // 屏幕宽
+    let screenWidth :CGFloat = UIScreen.main.bounds.size.width
+    // 屏幕高
+    let screenHeight :CGFloat = UIScreen.main.bounds.size.height
+    // 滑动标签页高度
+    let scrollLabelHeight :CGFloat = CGFloat(40)
+
+    // 滑动标签
+    var scrollLabaleView :UIScrollView?
+    // 滑动标签
+    var collectionView :UICollectionView?
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        catalogCollectionView.dataSource = self
-        catalogCollectionView.delegate = self
+        addScrollLabelView()
+        addCollectionView()
+    }
+    
+    func addScrollLabelView() {
+        // 创建
+        scrollLabaleView = UIScrollView.init(frame: self.view.bounds) // UIScreen.main.bounds
+        // 背景色
+        scrollLabaleView?.backgroundColor = UIColor.green
+        // x , y , width , height
+        scrollLabaleView?.frame = CGRect(x: 0, y: 0, width: screenWidth, height: scrollLabelHeight)
+        // ScrollView偏移量
+        // scrollLabaleView?.contentOffset = CGPoint(x:screenWidth, y:0)
+        // 是否按页滚动
+        // scrollLabaleView?.pagingEnabled = true
+        // 水平滚动，而不显示滚动条
+        scrollLabaleView?.alwaysBounceHorizontal = true
+        scrollLabaleView?.showsHorizontalScrollIndicator = false
+        scrollLabaleView?.showsVerticalScrollIndicator = false
+        // 协议
+        scrollLabaleView?.delegate = self
+        // 插入
+        self.view.addSubview(scrollLabaleView!)
+    }
 
-        coverCollectionView.dataSource = self
-        coverCollectionView.delegate = self
+    func addCollectionView () {
+
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width:80,height:35)
+        //列间距,行间距,偏移
+        layout.minimumInteritemSpacing = 15
+        layout.minimumLineSpacing = 30
+        layout.sectionInset = UIEdgeInsetsMake(10, 10, 10, 10)
+        
+        collectionView = UICollectionView.init(frame: self.view.bounds, collectionViewLayout: layout)
+        collectionView?.frame = CGRect(x: 0, y: scrollLabelHeight, width: screenWidth, height: screenWidth-scrollLabelHeight)
+        collectionView?.delegate = self
+        collectionView?.dataSource = self;
+        //注册一个cell
+        collectionView!.register(HotCell.self, forCellWithReuseIdentifier:"HotCell")
+        collectionView?.backgroundColor = UIColor.blue
+        self.view.addSubview(collectionView!)
+    }
+    
+    // 有多少个 cell
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 18;
+    }
+    
+    // 每个 cell 的处理
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        // default cover
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "HotCell", for: indexPath) as! HotCell
+        cell.backgroundColor = UIColor.red
+        cell.label.text = "aaaa"
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+}
+
+class HotCell: UICollectionViewCell {
     
-    // 有多少个 cell
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    var label = UILabel()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
         
-        // catalog
-        if (collectionView == catalogCollectionView) {
-            return 18;
-        }
-
-        // default cover
-        return 18;
+        label = UILabel.init(frame: self.bounds)
+        self.addSubview(label)
+        
     }
     
-    // 每个 cell 的处理
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-
-        // catalog
-        if (collectionView == catalogCollectionView) {
-
-            let catalogCell = collectionView.dequeueReusableCell(withReuseIdentifier: "catalogCell", for: indexPath) as! CatalogCell
-            catalogCell.catalogLabel.text = "\(indexPath.section):\(indexPath.row)"
-            return catalogCell
-        }
-
-        // default cover
-        let coverCell = collectionView.dequeueReusableCell(withReuseIdentifier: "coverCell", for: indexPath) as! CoverCell
-        coverCell.coverImage.image = UIImage(named:"normal")
-        return coverCell
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-}
-
-class CatalogCell: UICollectionViewCell {
-
-    @IBOutlet var catalogLabel: UILabel!
-    
-}
-
-class CoverCell: UICollectionViewCell {
-
-    @IBOutlet var coverImage: UIImageView!
     
 }
